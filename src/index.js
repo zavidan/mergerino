c49cd8acd0c9df5a3f78a3cd3f66ab71dc103e25
Zavier Boyd <zavier@boyd.org.nz>
2020-06-09 14:52:36 +1300

    created mergeMutate, duplicated test suite, made sure object pointers are equal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const assign = Object.assign || ((a, b) => (b && Object.keys(b).forEach(k => (a[k] = b[k])), a))
 
-const run = (isArr, copy, patch) => {
+const duplicateObject = (source) => assign({}, source)
+
+const run = (isArr, copy, patch, merge) => {
   const type = typeof patch
   if (patch && type === 'object') {
-    if (Array.isArray(patch)) for (const p of patch) copy = run(isArr, copy, p)
+    if (Array.isArray(patch))
+      for (const p of patch) copy = run(isArr, copy, p, merge)
     else {
       for (const k of Object.keys(patch)) {
         const val = patch[k]
@@ -11,7 +14,7 @@ const run = (isArr, copy, patch) => {
         else if (val === undefined) isArr && !isNaN(k) ? copy.splice(k, 1) : delete copy[k]
         else if (val === null || typeof val !== 'object' || Array.isArray(val)) copy[k] = val
         else if (typeof copy[k] === 'object') copy[k] = val === copy[k] ? val : merge(copy[k], val)
-        else copy[k] = run(false, {}, val)
+        else copy[k] = run(false, {}, val, merge)
       }
     }
   } else if (type === 'function') copy = patch(copy, merge)
@@ -20,7 +23,25 @@ const run = (isArr, copy, patch) => {
 
 const merge = (source, ...patches) => {
   const isArr = Array.isArray(source)
-  return run(isArr, isArr ? source.slice() : assign({}, source), patches)
+  return run(isArr, isArr ? source.slice() : duplicateObject(source), patches, merge)
+}
+
+const mergeMutate = (source, ...patches) => {
+  const isArr = Array.isArray(source)
+  const newstate = run(isArr, source, patches, mergeMutate)
+  if (newstate !== source) {
+    Object.keys(source).forEach((key) => {
+      if (!newstate.hasOwnProperty(key)) {
+        delete source[key]
+      }
+    })
+    assign(source, newstate)
+  }
+  return source
 }
 
 export default merge
+export {
+  merge,
+  mergeMutate
+}
--- test.js
+++ test.js
@@ -1,92 +1,230 @@
 #!/usr/bin/env node
+
 /* global require */
 const o = require('ospec')
 
 const depPath = './dist/mergerino.es5.js'
-const merge = require(depPath)
-
+const {
+  merge,
+  mergeMutate
+} = require(depPath)
 // create version that uses assign polyfill
 const save = Object.assign
 Object.assign = null
 delete require.cache[require.resolve(depPath)]
-const noAssignMerge = require(depPath)
+const noAssignMerge = (() => {
+  const {
+    merge
+  } = require(depPath)
+  return merge
+})()
 Object.assign = save
 
-o.spec('mergerino', () => {
+o.spec('mergerino immutable', () => {
   o('deleting works', () => {
-    const state = { prop: true, other: true, deep: { prop: 'foo' } }
+    const state = {
+      prop: true,
+      other: true,
+      deep: {
+        prop: 'foo'
+      }
+    }
     const newState = merge(state, {
       prop: undefined,
-      deep: { prop: undefined },
+      deep: {
+        prop: undefined
+      },
       fake: undefined, // deleting non existent key
       other: null
     })
-    o(newState).deepEquals({ other: null, deep: {} })
-    o(state).deepEquals({ prop: true, other: true, deep: { prop: 'foo' } })
+    o(newState).deepEquals({
+      other: null,
+      deep: {}
+    })
+    o(state).deepEquals({
+      prop: true,
+      other: true,
+      deep: {
+        prop: 'foo'
+      }
+    })
   })
   o('function sub works', () => {
-    const state = { age: 10, name: 'bob', obj: { prop: true } }
+    const state = {
+      age: 10,
+      name: 'bob',
+      obj: {
+        prop: true
+      }
+    }
     const newState = merge(state, {
       age: x => x * 10,
-      obj: () => ({ replaced: true }),
+      obj: () => ({
+        replaced: true
+      }),
       name: (x, m) => {
         o(m).equals(merge) // verify that merge is passed as second arg
         return x
       }
     })
-    o(newState).deepEquals({ age: 100, name: 'bob', obj: { replaced: true } })
-    o(state).deepEquals({ age: 10, name: 'bob', obj: { prop: true } })
+    o(newState).deepEquals({
+      age: 100,
+      name: 'bob',
+      obj: {
+        replaced: true
+      }
+    })
+    o(state).deepEquals({
+      age: 10,
+      name: 'bob',
+      obj: {
+        prop: true
+      }
+    })
   })
   o('deep function sub to uncreated object path', () => {
-    const state = { orig: true }
+    const state = {
+      orig: true
+    }
     const newState = merge(state, {
-      add: { stats: { count: x => (x == null ? 1 : x + 1) } }
+      add: {
+        stats: {
+          count: x => (x == null ? 1 : x + 1)
+        }
+      }
     })
-    o(newState).deepEquals({ orig: true, add: { stats: { count: 1 } } })
-    o(state).deepEquals({ orig: true })
+    o(newState).deepEquals({
+      orig: true,
+      add: {
+        stats: {
+          count: 1
+        }
+      }
+    })
+    o(state).deepEquals({
+      orig: true
+    })
   })
   o('add nested object', () => {
-    const state = { age: 10 }
-    const add = { sub: true }
-    const newState = merge(state, { add })
-    o(newState).deepEquals({ age: 10, add: { sub: true } })
+    const state = {
+      age: 10
+    }
+    const add = {
+      sub: true
+    }
+    const newState = merge(state, {
+      add
+    })
+    o(newState).deepEquals({
+      age: 10,
+      add: {
+        sub: true
+      }
+    })
     o(newState.add).notEquals(add)
     o(newState).notEquals(state)
   })
   o('deep merge objects', () => {
-    const state = { age: 10, sub: { sub: { prop: true } } }
-    const newState = merge(state, { sub: { sub: { newProp: true } } })
-    o(state).deepEquals({ age: 10, sub: { sub: { prop: true } } })
-    o(newState).deepEquals({ age: 10, sub: { sub: { prop: true, newProp: true } } })
+    const state = {
+      age: 10,
+      sub: {
+        sub: {
+          prop: true
+        }
+      }
+    }
+    const newState = merge(state, {
+      sub: {
+        sub: {
+          newProp: true
+        }
+      }
+    })
+    o(state).deepEquals({
+      age: 10,
+      sub: {
+        sub: {
+          prop: true
+        }
+      }
+    })
+    o(newState).deepEquals({
+      age: 10,
+      sub: {
+        sub: {
+          prop: true,
+          newProp: true
+        }
+      }
+    })
     o(newState).notEquals(state)
     o(newState.sub).notEquals(state.sub)
     o(newState.sub.sub).notEquals(state.sub.sub)
   })
   o('function patch', () => {
-    const state = { age: 10, foo: 'bar' }
+    const state = {
+      age: 10,
+      foo: 'bar'
+    }
     const newState = merge(state, (s, m) => {
       o(s).notEquals(state)
       o(s).deepEquals(state)
-      return merge(s, { prop: true })
+      return merge(s, {
+        prop: true
+      })
     })
-    o(newState).deepEquals({ age: 10, foo: 'bar', prop: true })
+    o(newState).deepEquals({
+      age: 10,
+      foo: 'bar',
+      prop: true
+    })
   })
   o('multi/array/falsy patches', () => {
-    const state = { foo: 'bar' }
+    const state = {
+      foo: 'bar'
+    }
     const newState = merge(
-      state,
-      { baz: 5 },
-      { hello: false },
-      [{ arr: [1, 2, 3] }, [[{ prop: true }]], false, null],
+      state, {
+        baz: 5
+      }, {
+        hello: false
+      },
+      [{
+          arr: [1, 2, 3]
+        },
+        [
+          [{
+            prop: true
+          }]
+        ], false, null
+      ],
       undefined,
       '',
       0,
       null,
-      (s, m) => m(s, { age: 10 }),
-      [[[[[[[{ age: x => x * 3 }]]]]]]]
+      (s, m) => m(s, {
+        age: 10
+      }),
+      [
+        [
+          [
+            [
+              [
+                [
+                  [{
+                    age: x => x * 3
+                  }]
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
     )
     o(newState).notEquals(state)
-    o(state).deepEquals({ foo: 'bar' })
+    o(state).deepEquals({
+      foo: 'bar'
+    })
     o(newState).deepEquals({
       foo: 'bar',
       baz: 5,
@@ -98,63 +236,453 @@ o.spec('mergerino', () => {
   })
   o('array patches', () => {
     const arr = [1, 2, 3]
-    const newArr = merge(arr, { 2: 100 }, { 0: undefined }, { 0: 200 })
+    const newArr = merge(arr, {
+      2: 100
+    }, {
+      0: undefined
+    }, {
+      0: 200
+    })
     o(newArr).notEquals(arr)
     o(newArr).deepEquals([200, 100])
     o(arr).deepEquals([1, 2, 3])
   })
   o('deep merge with arr', () => {
-    const state = { foo: 'bar', deep: { arr: [1, 2, 3], prop: false } }
-    const newState = merge(state, { deep: { arr: { 1: 20 } } })
+    const state = {
+      foo: 'bar',
+      deep: {
+        arr: [1, 2, 3],
+        prop: false
+      }
+    }
+    const newState = merge(state, {
+      deep: {
+        arr: {
+          1: 20
+        }
+      }
+    })
     o(newState.deep).notEquals(state.deep)
     o(newState.deep.arr).notEquals(state.deep.arr)
     o(newState.deep.arr).deepEquals([1, 20, 3])
     o(state.deep.arr).deepEquals([1, 2, 3])
   })
   o('top level SUB', () => {
-    const state = { age: 20, foo: 'bar' }
-    const replacement = { replaced: true }
+    const state = {
+      age: 20,
+      foo: 'bar'
+    }
+    const replacement = {
+      replaced: true
+    }
     const newState = merge(state, () => replacement)
     o(newState).notEquals(state)
     o(newState).equals(replacement)
   })
   o('reuse object if same ref when patching', () => {
-    const state = { deep: { prop: true } }
-    const newState = merge(state, { deep: state.deep })
+    const state = {
+      deep: {
+        prop: true
+      }
+    }
+    const newState = merge(state, {
+      deep: state.deep
+    })
     o(newState).notEquals(state) // TODO: maybe try and be smarter, to avoid copy if patch changes nothing
     o(newState.deep).equals(state.deep)
   })
   o('assign polyfill works', () => {
-    const state = { prop: true, deep: { prop: true, deeper: { foo: 'bar' } } }
+    const state = {
+      prop: true,
+      deep: {
+        prop: true,
+        deeper: {
+          foo: 'bar'
+        }
+      }
+    }
     let newState = noAssignMerge(
-      state,
-      { prop: false, deep: { deeper: { new: true } } },
+      state, {
+        prop: false,
+        deep: {
+          deeper: {
+            new: true
+          }
+        }
+      },
       false,
       null,
       '',
       0
     )
     o(newState).notEquals(state)
-    o(newState).deepEquals({ prop: false, deep: { prop: true, deeper: { foo: 'bar', new: true } } })
+    o(newState).deepEquals({
+      prop: false,
+      deep: {
+        prop: true,
+        deeper: {
+          foo: 'bar',
+          new: true
+        }
+      }
+    })
 
-    newState = noAssignMerge(void 0, { foo: 'bar' })
-    o(newState).deepEquals({ foo: 'bar' })
+    newState = noAssignMerge(void 0, {
+      foo: 'bar'
+    })
+    o(newState).deepEquals({
+      foo: 'bar'
+    })
   })
   o('multi function patch, only copy once', () => {
     const copies = []
-    merge({ key: 'value' }, Array.from({ length: 5 }, () => state => (copies.push(state), state)))
+    merge({
+      key: 'value'
+    }, Array.from({
+      length: 5
+    }, () => state => (copies.push(state), state)))
     o(copies.length).equals(5)
     o(typeof copies[0]).equals('object')
     copies.every(copy => o(copy).equals(copies[0]))
   })
   o('does not throw error for falsy source', () => {
-    const newState = merge(void 0, { foo: 'bar' })
-    o(newState).deepEquals({ foo: 'bar' })
+    const newState = merge(void 0, {
+      foo: 'bar'
+    })
+    o(newState).deepEquals({
+      foo: 'bar'
+    })
   })
   o('replace primitive with object and vice versa', () => {
-    const state = { count: 10, foo: { prop: true } }
-    const newState = merge(state, { count: { prop: true }, foo: 10 })
-    o(state).deepEquals({ count: 10, foo: { prop: true } })
-    o(newState).deepEquals({ count: { prop: true }, foo: 10 })
+    const state = {
+      count: 10,
+      foo: {
+        prop: true
+      }
+    }
+    const newState = merge(state, {
+      count: {
+        prop: true
+      },
+      foo: 10
+    })
+    o(state).deepEquals({
+      count: 10,
+      foo: {
+        prop: true
+      }
+    })
+    o(newState).deepEquals({
+      count: {
+        prop: true
+      },
+      foo: 10
+    })
+  })
+})
+
+o.spec('mergerino mutable', () => {
+  o('deleting works', () => {
+    const state = {
+      prop: true,
+      other: true,
+      deep: {
+        prop: 'foo'
+      }
+    }
+    const newState = mergeMutate(state, {
+      prop: undefined,
+      deep: {
+        prop: undefined
+      },
+      fake: undefined, // deleting non existent key
+      other: null
+    })
+    o(newState).deepEquals({
+      other: null,
+      deep: {}
+    })
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('function sub works', () => {
+    const state = {
+      age: 10,
+      name: 'bob',
+      obj: {
+        prop: true
+      }
+    }
+    const newState = mergeMutate(state, {
+      age: x => x * 10,
+      obj: () => ({
+        replaced: true
+      }),
+      name: (x, m) => {
+        o(m).equals(mergeMutate) // verify that mergeMutate is passed as second arg
+        return x
+      }
+    })
+    o(newState).deepEquals({
+      age: 100,
+      name: 'bob',
+      obj: {
+        replaced: true
+      }
+    })
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('deep function sub to uncreated object path', () => {
+    const state = {
+      orig: true
+    }
+    const newState = mergeMutate(state, {
+      add: {
+        stats: {
+          count: x => (x == null ? 1 : x + 1)
+        }
+      }
+    })
+    o(newState).deepEquals({
+      orig: true,
+      add: {
+        stats: {
+          count: 1
+        }
+      }
+    })
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('add nested object', () => {
+    const state = {
+      age: 10
+    }
+    const add = {
+      sub: true
+    }
+    const newState = mergeMutate(state, {
+      add
+    })
+    o(newState).deepEquals({
+      age: 10,
+      add: {
+        sub: true
+      }
+    })
+    o(newState.add).notEquals(add)
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('deep mergeMutate objects', () => {
+    const state = {
+      age: 10,
+      sub: {
+        sub: {
+          prop: true
+        }
+      }
+    }
+    const newState = mergeMutate(state, {
+      sub: {
+        sub: {
+          newProp: true
+        }
+      }
+    })
+    o(newState).deepEquals({
+      age: 10,
+      sub: {
+        sub: {
+          prop: true,
+          newProp: true
+        }
+      }
+    })
+    o(newState).equals(state)
+    o(newState.sub).equals(state.sub)
+    o(newState.sub.sub).equals(state.sub.sub)
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('function patch', () => {
+    const state = {
+      age: 10,
+      foo: 'bar'
+    }
+    const newState = mergeMutate(state, (s, m) => {
+      o(s).equals(state)
+      o(s).deepEquals(state)
+      return mergeMutate(s, {
+        prop: true
+      })
+    })
+    o(newState).deepEquals({
+      age: 10,
+      foo: 'bar',
+      prop: true
+    })
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('multi/array/falsy patches', () => {
+    const state = {
+      foo: 'bar'
+    }
+    const newState = mergeMutate(
+      state, {
+        baz: 5
+      }, {
+        hello: false
+      },
+      [{
+          arr: [1, 2, 3]
+        },
+        [
+          [{
+            prop: true
+          }]
+        ], false, null
+      ],
+      undefined,
+      '',
+      0,
+      null,
+      (s, m) => m(s, {
+        age: 10
+      }),
+      [
+        [
+          [
+            [
+              [
+                [
+                  [{
+                    age: x => x * 3
+                  }]
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    )
+    // o(newState).notEquals(state)
+    // o(state).deepEquals({ foo: 'bar' })
+    o(newState).deepEquals({
+      foo: 'bar',
+      baz: 5,
+      prop: true,
+      hello: false,
+      arr: [1, 2, 3],
+      age: 30
+    })
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('array patches', () => {
+    const arr = [1, 2, 3]
+    const newArr = mergeMutate(arr, {
+      2: 100
+    }, {
+      0: undefined
+    }, {
+      0: 200
+    })
+    o(newArr).equals(arr)
+    o(newArr).deepEquals([200, 100])
+    o(arr).notDeepEquals([1, 2, 3])
+    o(arr).deepEquals(newArr)
+  })
+  o('deep mergeMutate with arr', () => {
+    const state = {
+      foo: 'bar',
+      deep: {
+        arr: [1, 2, 3],
+        prop: false
+      }
+    }
+    const newState = mergeMutate(state, {
+      deep: {
+        arr: {
+          1: 20
+        }
+      }
+    })
+    o(newState.deep).equals(state.deep)
+    o(newState.deep.arr).equals(state.deep.arr)
+    o(newState.deep.arr).deepEquals([1, 20, 3])
+    o(state.deep.arr).notDeepEquals([1, 2, 3])
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('top level SUB', () => {
+    const state = {
+      age: 20,
+      foo: 'bar'
+    }
+    const replacement = {
+      replaced: true
+    }
+    const newState = mergeMutate(state, () => replacement)
+    o(newState).notEquals(replacement)
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('reuse object if same ref when patching', () => {
+    const state = {
+      deep: {
+        prop: true
+      }
+    }
+    const newState = mergeMutate(state, {
+      deep: state.deep
+    })
+    // o(newState).notEquals(state) // TODO: maybe try and be smarter, to avoid copy if patch changes nothing
+    o(newState.deep).equals(state.deep)
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+  o('multi function patch, only copy once', () => {
+    const copies = []
+    mergeMutate({
+      key: 'value'
+    }, Array.from({
+      length: 5
+    }, () => state => (copies.push(state), state)))
+    o(copies.length).equals(5)
+    o(typeof copies[0]).equals('object')
+    copies.every(copy => o(copy).equals(copies[0]))
+  })
+  o('throws error for falsy source', () => {
+    o(() => {
+      const newState = mergeMutate(void 0, {
+        foo: 'bar'
+      })
+    }).throws(TypeError)
+  })
+  o('replace primitive with object and vice versa', () => {
+    const state = {
+      count: 10,
+      foo: {
+        prop: true
+      }
+    }
+    const newState = mergeMutate(state, {
+      count: {
+        prop: true
+      },
+      foo: 10
+    })
+    // o(state).deepEquals({ count: 10, foo: { prop: true } })
+    o(newState).deepEquals({
+      count: {
+        prop: true
+      },
+      foo: 10
+    })
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
   })
 })
