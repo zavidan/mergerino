ee280a8b85df838d5b536011529e26dd27b67ab7
Zavier Boyd <zavier@boyd.org.nz>
2020-06-10 10:02:57 +1300

    created a shallow merge that keeps the source pointer the same

--- src/index.js
+++ src/index.js
@@ -2,28 +2,29 @@ const assign = Object.assign || ((a, b) => (b && Object.keys(b).forEach(k => (a[
 
 const duplicateObject = (source) => assign({}, source)
 
-const run = (isArr, copy, patch, merge) => {
+const run = (isArr, copy, patch, mergeFunc = merge) => {
   const type = typeof patch
   if (patch && type === 'object') {
     if (Array.isArray(patch))
-      for (const p of patch) copy = run(isArr, copy, p, merge)
+      for (const p of patch) copy = run(isArr, copy, p, mergeFunc)
     else {
       for (const k of Object.keys(patch)) {
         const val = patch[k]
-        if (typeof val === 'function') copy[k] = val(copy[k], merge)
+        if (typeof val === 'function') copy[k] = val(copy[k], mergeFunc)
         else if (val === undefined) isArr && !isNaN(k) ? copy.splice(k, 1) : delete copy[k]
         else if (val === null || typeof val !== 'object' || Array.isArray(val)) copy[k] = val
-        else if (typeof copy[k] === 'object') copy[k] = val === copy[k] ? val : merge(copy[k], val)
-        else copy[k] = run(false, {}, val, merge)
+        else if (typeof copy[k] === 'object') copy[k] = val === copy[k] ? val : mergeFunc(copy[k], val)
+        else copy[k] = run(false, {}, val, mergeFunc)
       }
     }
-  } else if (type === 'function') copy = patch(copy, merge)
+  } else if (type === 'function') copy = patch(copy, mergeFunc)
   return copy
 }
 
 const merge = (source, ...patches) => {
   const isArr = Array.isArray(source)
-  return run(isArr, isArr ? source.slice() : duplicateObject(source), patches, merge)
+  const sourceCopy = isArr ? source.slice() : assign({}, source)
+  return run(isArr, sourceCopy, patches)
 }
 
 const mergeMutate = (source, ...patches) => {
@@ -40,8 +41,23 @@ const mergeMutate = (source, ...patches) => {
   return source
 }
 
+const mergeMutateTop = (source, ...patches) => {
+  const isArr = Array.isArray(source)
+  const newstate = run(isArr, source, patches)
+  if (newstate !== source) {
+    Object.keys(source).forEach((key) => {
+      if (!newstate.hasOwnProperty(key)) {
+        delete source[key]
+      }
+    })
+    assign(source, newstate)
+  }
+  return source
+}
+
 export default merge
 export {
   merge,
-  mergeMutate
+  mergeMutate,
+  mergeMutateTop
 }
--- test.js
+++ test.js
@@ -6,7 +6,8 @@ const o = require('ospec')
 const depPath = './dist/mergerino.es5.js'
 const {
   merge,
-  mergeMutate
+  mergeMutate,
+  mergeMutateTop
 } = require(depPath)
 // create version that uses assign polyfill
 const save = Object.assign
@@ -683,6 +684,312 @@ o.spec('mergerino mutable', () => {
       foo: 10
     })
     o(state).deepEquals(newState)
+    o(state).equals(newState)
+  })
+})
+
+o.spec('mergerino shallow mutable', () => {
+  o('deleting works', () => {
+    const state = {
+      prop: true,
+      other: true,
+      deep: {
+        prop: 'foo'
+      }
+    }
+    const test = Object.assign({}, state)
+    const newState = mergeMutateTop(state, {
+      prop: undefined,
+      deep: {
+        prop: undefined
+      },
+      fake: undefined, // deleting non existent key
+      other: null
+    })
+    o(newState).deepEquals({
+      other: null,
+      deep: {}
+    })
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+    o(state.deep).notEquals(test.deep)
+  })
+  o('function sub works', () => {
+    const state = {
+      age: 10,
+      name: 'bob',
+      obj: {
+        prop: true
+      }
+    }
+    const test = Object.assign({}, state)
+    const newState = mergeMutateTop(state, {
+      age: x => x * 10,
+      obj: () => ({
+        replaced: true
+      }),
+      name: (x, m) => {
+        o(m).equals(merge) // verify that merge is passed as second arg
+        return x
+      }
+    })
+    o(newState).deepEquals({
+      age: 100,
+      name: 'bob',
+      obj: {
+        replaced: true
+      }
+    })
+    o(state).notDeepEquals({
+      age: 10,
+      name: 'bob',
+      obj: {
+        prop: true
+      }
+    })
+    o(state).deepEquals(newState)
+    o(state).equals(newState)
+    o(state.obj).notEquals(test.obj)
+  })
+  o('deep function sub to uncreated object path', () => {
+    const state = {
+      orig: true
+    }
+    const newState = mergeMutateTop(state, {
+      add: {
+        stats: {
+          count: x => (x == null ? 1 : x + 1)
+        }
+      }
+    })
+    o(newState).deepEquals({
+      orig: true,
+      add: {
+        stats: {
+          count: 1
+        }
+      }
+    })
+    o(state).deepEquals(newState)
+  })
+  o('add nested object', () => {
+    const state = {
+      age: 10
+    }
+    const add = {
+      sub: true
+    }
+    const newState = mergeMutateTop(state, {
+      add
+    })
+    o(newState).deepEquals({
+      age: 10,
+      add: {
+        sub: true
+      }
+    })
+    o(newState.add).notEquals(add)
+    o(newState).equals(state)
+  })
+  o('deep merge objects', () => {
+    const state = {
+      age: 10,
+      sub: {
+        sub: {
+          prop: true
+        }
+      }
+    }
+    const test = Object.assign({}, state)
+    const newState = mergeMutateTop(state, {
+      sub: {
+        sub: {
+          newProp: true
+        }
+      }
+    })
+    o(state).equals(newState)
+    o(newState).deepEquals({
+      age: 10,
+      sub: {
+        sub: {
+          prop: true,
+          newProp: true
+        }
+      }
+    })
+    o(newState.sub).notEquals(test.sub)
+    o(newState.sub.sub).notEquals(test.sub.sub)
+  })
+  o('function patch', () => {
+    const state = {
+      age: 10,
+      foo: 'bar'
+    }
+    const newState = mergeMutateTop(state, (s, m) => {
+      o(s).equals(state)
+      return merge(s, {
+        prop: true
+      })
+    })
+    o(newState).deepEquals({
+      age: 10,
+      foo: 'bar',
+      prop: true
+    })
+  })
+  o('multi/array/falsy patches', () => {
+    const state = {
+      foo: 'bar'
+    }
+    const newState = mergeMutateTop(
+      state, {
+        baz: 5
+      }, {
+        hello: false
+      },
+      [{
+          arr: [1, 2, 3]
+        },
+        [
+          [{
+            prop: true
+          }]
+        ], false, null
+      ],
+      undefined,
+      '',
+      0,
+      null,
+      (s, m) => m(s, {
+        age: 10
+      }),
+      [
+        [
+          [
+            [
+              [
+                [
+                  [{
+                    age: x => x * 3
+                  }]
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    )
+    o(newState).equals(state)
+    o(state).notDeepEquals({
+      foo: 'bar'
+    })
+    o(newState).deepEquals({
+      foo: 'bar',
+      baz: 5,
+      prop: true,
+      hello: false,
+      arr: [1, 2, 3],
+      age: 30
+    })
+  })
+  o('array patches', () => {
+    const arr = [1, 2, 3]
+    const newArr = mergeMutateTop(arr, {
+      2: 100
+    }, {
+      0: undefined
+    }, {
+      0: 200
+    })
+    o(newArr).equals(arr)
+    o(newArr).deepEquals([200, 100])
+  })
+  o('deep merge with arr', () => {
+    const state = {
+      foo: 'bar',
+      deep: {
+        arr: [1, 2, 3],
+        prop: false
+      }
+    }
+    const test = Object.assign({}, state)
+    const newState = mergeMutateTop(state, {
+      deep: {
+        arr: {
+          1: 20
+        }
+      }
+    })
+    o(newState.deep).notEquals(test.deep)
+    o(newState.deep.arr).notEquals(test.deep.arr)
+    o(newState.deep.arr).deepEquals([1, 20, 3])
+    o(test.deep.arr).deepEquals([1, 2, 3])
+  })
+  o('top level SUB', () => {
+    const state = {
+      age: 20,
+      foo: 'bar'
+    }
+    const replacement = {
+      replaced: true
+    }
+    const newState = mergeMutateTop(state, () => replacement)
+    o(newState).equals(state)
+    o(newState).notEquals(replacement)
+  })
+  o('reuse object if same ref when patching', () => {
+    const state = {
+      deep: {
+        prop: true
+      }
+    }
+    const test = Object.assign({}, state)
+    const newState = mergeMutateTop(state, {
+      deep: test.deep
+    })
+    o(newState).equals(state) // TODO: maybe try and be smarter, to avoid copy if patch changes nothing
+    o(newState.deep).equals(test.deep)
+  })
+  o('multi function patch, only copy once', () => {
+    const copies = []
+    mergeMutateTop({
+      key: 'value'
+    }, Array.from({
+      length: 5
+    }, () => state => (copies.push(state), state)))
+    o(copies.length).equals(5)
+    o(typeof copies[0]).equals('object')
+    copies.every(copy => o(copy).equals(copies[0]))
+  })
+  o('throw error for falsy source', () => {
+    o(() => {
+      const newState = mergeMutateTop(void 0, {
+        foo: 'bar'
+      })
+    }).throws(TypeError)
+  })
+  o('replace primitive with object and vice versa', () => {
+    const state = {
+      count: 10,
+      foo: {
+        prop: true
+      }
+    }
+    const test = Object.assign({}, state)
+    const newState = mergeMutateTop(state, {
+      count: {
+        prop: true
+      },
+      foo: 10
+    })
+    o(state.foo).notEquals(test.foo)
+    o(newState).deepEquals({
+      count: {
+        prop: true
+      },
+      foo: 10
+    })
     o(state).equals(newState)
   })
 })
